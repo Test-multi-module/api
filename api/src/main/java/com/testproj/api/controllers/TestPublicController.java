cf9946b8ed5e92babacd060feb63b132756af92b
Yana <yana.tereshchenko@softpos.eu>
2025-06-19 14:23:29 +0200

    root controller

--- api/src/main/java/com/testproj/api/controllers/TestPublicController.java
+++ api/src/main/java/com/testproj/api/controllers/TestPublicController.java
@@ -1,11 +1,13 @@
 package com.testproj.api.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class TeslPublicController {
-    @GetMapping("/")
+@RequestMapping("/")
+public class TestPublicController {
+    @GetMapping()
     public String home() {
         return "App is running!";
     }
