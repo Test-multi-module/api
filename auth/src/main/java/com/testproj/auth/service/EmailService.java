ad15741cdbb37a48f90179cbfb24ceb7a03f1278
Yana <yana.tereshchenko@softpos.eu>
2025-06-20 17:29:25 +0200

    nothing important

--- auth/src/main/java/com/testproj/auth/service/EmailService.java
+++ auth/src/main/java/com/testproj/auth/service/EmailService.java
@@ -1,7 +1,6 @@
 package com.testproj.auth.service;
 
 
-import com.testproj.auth.security.JwtService;
 import lombok.AllArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
