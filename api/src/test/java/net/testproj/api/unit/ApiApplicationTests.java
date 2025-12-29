4e4f26f0a9a46530ecd8663db98fa3bb1fdc6d58
yana <tereshenko24102000@gmail.com>
2025-12-29 15:24:41 +0100

    недоделанные тесты

--- api/src/test/java/net/testproj/api/unit/ApiApplicationTests.java
+++ api/src/test/java/net/testproj/api/unit/ApiApplicationTests.java
@@ -1,19 +1,56 @@
 package net.testproj.api.unit;
 
-import net.testproj.api.ApiApplication;
-import net.testproj.api.controllers.TestControllerToCheckHowToUseDiffAnnotations;
+import lombok.RequiredArgsConstructor;
+import net.testproj.auth.handlers.CustomOAuth2SuccessHandler;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 
+import java.io.IOException;
+import java.time.Instant;
+import java.util.List;
+import java.util.Map;
 
-@SpringBootTest(classes = ApiApplication.class)
+@SpringBootTest
 class ApiApplicationTests {
+    @Autowired private CustomOAuth2SuccessHandler handler;
 
-    @Autowired
-    TestControllerToCheckHowToUseDiffAnnotations testControllerToCheckHowToUseDiffAnnotations;
+    @Test
+    void successHandlerLogic() throws IOException {
+        Map<String, Object> claims = Map.of(
+                "sub", "123",
+                "email", "test@example.com",
+                "name", "Test User"
+        );
+        Instant now = Instant.now();
+        OAuth2AuthenticationToken auth = getOAuth2AuthenticationToken(now, claims);
 
-    public static void main(String[] args) {
-        String classpath = System.getProperty("java.class.path");
-        System.out.println("Classpath: " + classpath);
+        var request = new MockHttpServletRequest();
+        var response = new MockHttpServletResponse();
+
+        handler.onAuthenticationSuccess(request, response, auth);
+    }
+
+    private static @NonNull OAuth2AuthenticationToken getOAuth2AuthenticationToken(Instant now, Map<String, Object> claims) {
+        OidcIdToken idToken =
+                new OidcIdToken("fake-id-token", now, now.plusSeconds(3600), claims);
+
+        OidcUserInfo userInfo = new OidcUserInfo(claims);
+
+        OidcUser oAuth2User = new DefaultOidcUser(
+                List.of(new OidcUserAuthority(idToken, userInfo)),
+                idToken, userInfo, "sub");
+
+        return new OAuth2AuthenticationToken(
+                oAuth2User, oAuth2User.getAuthorities(), "google");
     }
 }
