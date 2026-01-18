861d905386f24c8b6673467d51440fa3c63ce108
yana <tereshenko24102000@gmail.com>
2026-01-18 17:11:29 +0100

    clean login&password flow (not actual at the moment but disturb of understanding)

--- api/src/main/java/net/testproj/api/config/SecurityConfig.java
+++ api/src/main/java/net/testproj/api/config/SecurityConfig.java
@@ -1,18 +1,13 @@
 package net.testproj.api.config;
 
 import net.testproj.auth.handlers.CustomOAuth2SuccessHandler;
-import net.testproj.auth.security.AuthUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -20,30 +15,18 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableMethodSecurity
 @AllArgsConstructor
 public class SecurityConfig {
-
-    private final AuthUserService authUserService;
     private final CustomOAuth2SuccessHandler successHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authz -> authz
+        return http.authorizeHttpRequests(authz -> authz
                         .requestMatchers("/api/private/**").authenticated()
                         .anyRequest().permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
                 .oauth2Login(oauth2 -> oauth2.successHandler(successHandler))
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
-
-        return http.build();
-    }
-
-    @Bean public PasswordEncoder passwordEncoder() {return new BCryptPasswordEncoder();}
-
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder.userDetailsService(this.authUserService).passwordEncoder(passwordEncoder());
-        return authenticationManagerBuilder.build();
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}))
+                .build();
     }
 }
