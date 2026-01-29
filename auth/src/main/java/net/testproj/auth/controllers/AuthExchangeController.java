43d123f28718a8243e4538a9d9c9ec6d6306d942
yana <tereshenko24102000@gmail.com>
2026-01-29 16:43:15 +0100

    test profile settings and todo for exchange endpoint

---
+++ auth/src/main/java/net/testproj/auth/controllers/AuthExchangeController.java
@@ -0,0 +1,13 @@
+package net.testproj.auth.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth/exchange")
+public class AuthExchangeController {
+    /*todo endpoint
+    * не забыть когда буду разрабатывать что это публичный контролле #permitAll
+    * будет postMapping
+    * */
+}
