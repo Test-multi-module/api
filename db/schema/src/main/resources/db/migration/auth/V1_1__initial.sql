0a5f69216a1a82831b844d14372e8659e63fe9aa
yana <tereshenko24102000@gmail.com>
2026-07-06 20:20:51 +0100

    schemes cleaning

--- db/schema/src/main/resources/db/migration/auth/V1_1__initial.sql
+++ db/schema/src/main/resources/db/migration/auth/V1_1__initial.sql
@@ -1,12 +1,5 @@
 CREATE TABLE users (
-    id uuid NOT NULL,
-
-    created_at timestamptz  NOT NULL default now(),
-    updated_at timestamptz  NOT NULL default now(),
-    disabled_at timestamptz,
-    deleted_at timestamptz,
-
-    email character varying(255)
+    id uuid NOT NULL
 );
 
 ALTER TABLE users ADD CONSTRAINT users_pkey PRIMARY KEY (id);
@@ -15,20 +8,12 @@ CREATE TABLE oauth2_accounts(
     id uuid NOT NULL,
     user_id uuid NOT NULL,
 
-    provider varchar(32),
+    provider varchar(32) NOT NULL,
     provider_user_id varchar(255) NOT NULL,
-    email_at_provider varchar(254) NOT NULL,
 
+    email_at_provider varchar(254) NOT NULL,
     email_verified boolean NOT NULL default FALSE,
 
-    created_at timestamptz  NOT NULL default now(),
-    updated_at timestamptz  NOT NULL default now(),
-    disabled_at timestamptz,
-    deleted_at timestamptz,
-
-    last_login_at timestamptz,
-
-    provider_avatar_url text,
     given_name text,
     family_name text
 );
@@ -39,5 +24,3 @@ ALTER TABLE oauth2_accounts ADD CONSTRAINT oauth2_accounts_fkey
     FOREIGN KEY (user_id) REFERENCES users(id);
 ALTER TABLE oauth2_accounts ADD CONSTRAINT oauth2_accounts_uq_provider_provider_user_id
     UNIQUE(provider, provider_user_id);
-ALTER TABLE oauth2_accounts ADD CONSTRAINT  oauth2_accounts_uq_provider_user_id
-    UNIQUE(provider, user_id);
--- db/schema/src/main/resources/db/migration/public/V2_1__test.sql
+++ db/schema/src/main/resources/db/migration/public/V2_1__test.sql
@@ -1,12 +1,7 @@
 create table  users(
     id uuid NOT NULL,
     nick_name character varying(64) NOT NULL,
-    day_of_birth timestamptz,
-    auth_user_id UUID UNIQUE NOT NULL,
-    provider_avatar_url text
+    day_of_birth timestamptz
 );
 
 ALTER TABLE users ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-ALTER TABLE users ADD CONSTRAINT users_fkey
-    FOREIGN KEY (auth_user_id) REFERENCES auth.users(id) ON DELETE CASCADE;
